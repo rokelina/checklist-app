54807c36e321e61da6259728f2a4cd8a80229d1b
Rosina Scampino <rosinascampino@gmail.com>
2023-01-13 11:44:35 -0500

    addTask handler and checkbox handler

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+
+// const tasks array of tasks objects[]
+
+//const tasksObjects id, title, isChecked: false, isDeleted: false
+  //event handler to tack isChecked
+  //event handler to track isDeleted
+//form input with a clickable button. Event handler onClick adds a taskObject to the array
+
 
 function App() {
+  const [tasks, setTask] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTaskClick = () => {
+    const newTask = {
+      title: inputValue,
+      isChecked: false,
+      isDeleted: false
+    };
+
+    const newTaskArray = [...tasks, newTask];
+    setTask(newTaskArray);
+    setInputValue("");
+  };
+
+  const toggleChecked = (index) => {
+    const newTaskArray = [...tasks];
+    newTaskArray[index].isChecked = !newTaskArray[index].isChecked;
+    setTask(newTaskArray);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="task-container">
+        <div className="add-task">
+          <input
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+            className="add-item-input"
+            placeholder="Add a task..."
+          ></input>
+          <button onClick={handleAddTaskClick}>+</button>
+        </div>
+        <div className="task-list">
+          {tasks.map((task, index) => (
+            <div key={index} className="task-container">
+              <input
+                type="checkbox"
+                onChange={() => toggleChecked(index)}
+              ></input>
+              <label>{task.title}</label>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
